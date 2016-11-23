fd33b33c0c28ac1d97114b2a1c5afe71c72ef95f
joselaradelamo <joselaradelamo>
2016-11-23 21:07:29 +0100

    :bug:

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.intent("writeIntent",
 		]
 	},
 	function(request,response) {
-		twitterClient.post('statuses/update', {status: 'Hi! I\'m testing twitter with alexa'!},  function(error, tweet, response) {
+		twitterClient.post('statuses/update', {status: 'Hi! I\'m testing twitter with alexa!'},  function(error, tweet, response) {
 		  	if(error) throw error;
 		  	console.log(tweet);  // Tweet body. 
 		  	console.log(response);  // Raw response object. 
